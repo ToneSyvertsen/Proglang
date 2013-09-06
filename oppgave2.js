656d9e703838168b6830b5e1926f522e60784e44
ToneSyvertsen <tonesyvertsen@Tones-MacBook-Air.local>
2013-09-06 23:20:58 +0200

    Til LABoppgavene i IS-104

---
+++ oppgave2.js
@@ -0,0 +1,7 @@
+
+function displayChoice()
+{
+document.getElementById("outputbox").innerHTML = document.getElementById("spraak").options[document.getElementById("spraak").selectedIndex].value;
+}
+
+ 
